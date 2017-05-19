c14f4d75424900f302b640b2f719826ab0c9ad44
Ann Dai <gofarann@gmail.com>
2017-05-19 15:23:12 -0700

    wrote display and remove seconds functions

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+$(document).ready(function() {
+// hours
+var displayHours = function() {
+  var time = new Date();
+  timeString = '<h1>' + time.getMinutes() + '</h1>';
+  nothing = "";
+  $("#hours").append(timeString);
+};
+
+var removeHours = function() {
+  var time = new Date();
+  timeString = '<h1>' + time.getMinutes() + '</h1>';
+  nothing = "";
+  $("#hours").children('h1').remove();
+};
+
+
+var t = setInterval(displayHours, 1000);
+var x = setInterval(removeHours, 999);
+
+// minutes
+
+// seconds
+  var displaySecs = function() {
+    var time = new Date();
+    timeString = '<h1>' + time.getSeconds() + '</h1>';
+    nothing = "";
+    $("#seconds").append(timeString);
+  };
+
+  var removeSecs = function() {
+    var time = new Date();
+    timeString = '<h1>' + time.getSeconds() + '</h1>';
+    nothing = "";
+    $("#seconds").children('h1').remove();
+  };
+
+
+  var t = setInterval(displaySecs, 1000);
+  var x = setInterval(removeSecs, 999);
+
+
+
+});
