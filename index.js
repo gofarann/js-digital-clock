de75905b2e17b4a8f3b3c97457d062f3f3f5c0a8
Ann Dai <gofarann@gmail.com>
2017-05-19 15:46:39 -0700

    added am/pm and 0

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 // Your code here
 $(document).ready(function() {
 // hours
-var displayHours = function() {
-  var time = new Date();
-  timeString = '<h1>' + time.getMinutes() + '</h1>';
-  nothing = "";
-  $("#hours").append(timeString);
-};
-
-var removeHours = function() {
-  var time = new Date();
-  timeString = '<h1>' + time.getMinutes() + '</h1>';
-  nothing = "";
-  $("#hours").children('h1').remove();
-};
-
-
-var t = setInterval(displayHours, 1000);
-var x = setInterval(removeHours, 999);
-
-// minutes
+// var displayHours = function() {
+//   var time = new Date();
+//   timeString = '<h1>' + time.getHours() + '</h1>';
+//   nothing = "";
+//   $("#hours").append(timeString);
+// };
+//
+// var removeHours = function() {
+//   $("#hours").children('h1').remove();
+// };
+//
+//
+// var t = setInterval(displayHours, 1000);
+// var x = setInterval(removeHours, 999);
+//
+// // minutes
+//
+// var displayMinutes = function() {
+//   var time = new Date();
+//   timeString = '<h1>' + time.getMinutes() + '</h1>';
+//   nothing = "";
+//   $("#minutes").append(timeString);
+// };
+//
+// var removeMinutes = function() {
+//   $("#minutes").children('h1').remove();
+// };
+//
+//
+// var t = setInterval(displayMinutes, 1000);
+// var x = setInterval(removeMinutes, 999);
 
 // seconds
-  var displaySecs = function() {
-    var time = new Date();
-    timeString = '<h1>' + time.getSeconds() + '</h1>';
-    nothing = "";
-    $("#seconds").append(timeString);
-  };
+var displaySecs = function() {
+  var time = new Date();
+  var ampm = time.getHours() >= 12 ? ' PM' : ' AM';
+  var zero = time.getSeconds() < 10 ? '0' : '';
 
-  var removeSecs = function() {
-    var time = new Date();
-    timeString = '<h1>' + time.getSeconds() + '</h1>';
-    nothing = "";
-    $("#seconds").children('h1').remove();
-  };
+  timeString = '<h1>' + time.getHours() + ":" + time.getMinutes() +  ":" + zero + time.getSeconds() + ampm + '</h1>';
+  $("#seconds").append(timeString);
+};
+
+var removeSecs = function() {
+  $("#seconds").children('h1').remove();
+};
 
 
-  var t = setInterval(displaySecs, 1000);
-  var x = setInterval(removeSecs, 999);
+var t = setInterval(displaySecs, 1000);
+var x = setInterval(removeSecs, 999);
 
 
 
