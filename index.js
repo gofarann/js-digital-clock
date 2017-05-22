f2ae21e47f57d4dd28c9b4133fe72a7d938cc12e
Ann Dai <gofarann@gmail.com>
2017-05-21 18:40:32 -0700

    attempted to created timezones, setInterval does not work with callbacks

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
+// // Your code here
+// $(document).ready(function() {
+//
+// var displaySecs = function() {
+//   var time = new Date();
+//   var ampm = time.getHours() >= 12 ? 'PM' : 'AM';
+//   var secondZero = time.getSeconds() < 10 ? '0' : '';
+//   var minuteZero = time.getMinutes() < 10 ? '0' : '';
+//
+//
+//   timeString = '<h1>' + time.getHours() + ":" + minuteZero + time.getMinutes() +  ":" + secondZero + time.getSeconds() + ampm + '</h1>';
+//
+//   $("#clock").html(timeString);
+// };
+//
+// var t = setInterval(displaySecs, 1000);
+//
+//
+//
+// });
+
+
 // Your code here
 $(document).ready(function() {
 
-var displaySecs = function() {
-  var time = new Date();
-  var ampm = time.getHours() >= 12 ? 'PM' : 'AM';
-  var secondZero = time.getSeconds() < 10 ? '0' : '';
-  var minuteZero = time.getMinutes() < 10 ? '0' : '';
+  var timeString = function(location, offset) {
+    var time = new Date();
+    var ampm = time.getHours() >= 12 ? 'PM' : 'AM';
+    var secondZero = time.getSeconds() < 10 ? '0' : '';
+    var minuteZero = time.getMinutes() < 10 ? '0' : '';
+    timeString = '<h1>'+ location + " " + time.getHours() + ":" + minuteZero + time.getMinutes() +  ":" + secondZero + time.getSeconds() + ampm + '</h1>';
+    return timeString;
+  };
 
+  var displayTime = function(location, offset, callback) {
+    location = callback(location, offset);
+    $("#clock").html(location);
+  };
 
-  timeString = '<h1>' + time.getHours() + ":" + minuteZero + time.getMinutes() +  ":" + secondZero + time.getSeconds() + ampm + '</h1>';
-
-  $("#clock").html(timeString);
-};
-
-var t = setInterval(displaySecs, 1000);
-
-
+  var t = setInterval(displayTime("Seattle", 0, timeString), 1000);
 
 });
