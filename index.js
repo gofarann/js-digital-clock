6938dd2034897699eabad5d9ffb3c93dfb1808f7
Ann Dai <gofarann@gmail.com>
2017-05-21 17:33:20 -0700

    deleted superfluous js code

--- index.js
+++ index.js
@@ -1,39 +1,6 @@
 // Your code here
 $(document).ready(function() {
-// hours
-// var displayHours = function() {
-//   var time = new Date();
-//   timeString = '<h1>' + time.getHours() + '</h1>';
-//   nothing = "";
-//   $("#hours").append(timeString);
-// };
-//
-// var removeHours = function() {
-//   $("#hours").children('h1').remove();
-// };
-//
-//
-// var t = setInterval(displayHours, 1000);
-// var x = setInterval(removeHours, 999);
-//
-// // minutes
-//
-// var displayMinutes = function() {
-//   var time = new Date();
-//   timeString = '<h1>' + time.getMinutes() + '</h1>';
-//   nothing = "";
-//   $("#minutes").append(timeString);
-// };
-//
-// var removeMinutes = function() {
-//   $("#minutes").children('h1').remove();
-// };
-//
-//
-// var t = setInterval(displayMinutes, 1000);
-// var x = setInterval(removeMinutes, 999);
 
-// seconds
 var displaySecs = function() {
   var time = new Date();
   var ampm = time.getHours() >= 12 ? 'PM' : 'AM';
@@ -43,15 +10,7 @@ var displaySecs = function() {
 
   $("#clock").html(timeString);
 };
-//
-// var removeSecs = function() {
-//   $("#seconds").children('h1').remove();
-// };
-
 
 var t = setInterval(displaySecs, 1000);
-// var x = setInterval(removeSecs, 999);
-
-
 
 });
